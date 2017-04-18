b8932e172b9b4ccd68f3cb9f1d189dd13004c950
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-04-18 11:28:53 -0400

    This is my first python commit

---
+++ python101.py
@@ -0,0 +1,7 @@
+print "Hello, World!"
+# Print two different things on the same line
+print ("Hello, World", "Again")
+
+print """This will ignore the new lines
+until it sees another
+three quotes"""
